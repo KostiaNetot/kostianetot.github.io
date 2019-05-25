9738b351a823265696d844e3844fefcfaae26b5e
pugjazz@gmail.com <pugjazz@gmail.com>
2019-05-25 12:53:44 +0300

    modal trans

--- proj_faline/js/main.js
+++ proj_faline/js/main.js
@@ -1,3 +1,4 @@
+/*-----Appearence of Modal Window-----*/
 var modal = document.querySelector('#modal');
 var lessonBtn = document.querySelector('#lesson-btn');
 var close = document.querySelector('#close');
@@ -14,4 +15,11 @@ window.onclick = function (event) {
 	}
 }
 
-console.log(close);
+/*------TextArea Translate-------*/
+var modalContent = document.querySelector('.modal-content');
+var textAr = document.querySelector('#textarea');
+textAr.addEventListener('focus', transUp);
+
+function transUp() {
+	modalContent.style.transform = 'translateY(-50%)';
+}
