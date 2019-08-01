c8c95c11c6ce543bd615b98d82349e71fd9be5e7
pugjazz@gmail.com <pugjazz@gmail.com>
2019-08-01 13:37:06 +0300

    form works I hope so

--- mail.php
+++ mail.php
@@ -1,13 +1,13 @@
 <?php
 
 $recepient = "pugjazz@gmail.com";
-$sitename = "Form from site: Dmitry Rotkin";
+$sitename = "Dmitry Rotkin";
 
 $name = trim($_POST["name"]);
 $phone = trim($_POST["phone"]);
 $email = trim($_POST["email"]);
 $text = trim($_POST["text"]);
-$message = "Имя: $name \nТелефон: $phone \nТекст: $text";
+$message = "Guest's Name: $name \nGuest's Phone: $phone \nGuest's Email: $email \nGuest's Message: $text";
 
-$pagetitle = "Новая заявка с сайта \"$sitename\"";
+$pagetitle = "Form from site:  \"$sitename\"";
 mail($recepient, $pagetitle, $message, "Content-type: text/plain; charset=\"utf-8\"\n From: $recepient");
